5c4d3b96632f7d914bbf09a965aeb393fc003bd3
f3liz <felixchen14@gmail.com>
2025-05-06 14:51:11 -0700

    live code done

--- src/main/java/io/github/auberonedu/equalsLivecode/App.java
+++ src/main/java/io/github/auberonedu/equalsLivecode/App.java
@@ -17,7 +17,19 @@ public class App {
         System.out.println("Result of locA == locB " + (locA == locB));
         System.out.println("Result of locA.equals(locB) " + locA.equals(locB));
 
-        videoDemo();
+        Set<Centroid> centroids = new HashSet<>();
+        centroids.add(locA);
+        centroids.add(locB);
+        System.out.println(centroids.contains(locB));
+        System.out.println(locA.hashCode());
+        System.out.println(locB.hashCode());
+        
+        Location a = new Location(5, 1, "food");
+        Location b = new Location(5, 1, "food");
+
+        System.out.println(a.equals(b));
+
+        // videoDemo();
     }
 
     // We will look at this a bit later in the livecode, please ignore for first part
